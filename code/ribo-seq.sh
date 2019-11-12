36bc21f97ca1a7bf129490a882b053f7eb09791c
lijing28101 <jingli@iastate.edu>
2019-11-12 17:00:11 -0600

    Update ribo-seq.sh

--- code/ribo-seq.sh
+++ code/ribo-seq.sh
@@ -10,16 +10,12 @@
 # Indexes: STAR index, bowtie2 rRNA index
 
 module load bowtie2/2.3.1-py3-ge4lv4s
-module load star/2.5.3a-rdzqclb
+module load kallisto
 module load python/3.6.3-u4oaxsb
 module load py-pip/9.0.1-py3-dpds55c
 
 #copy needed files to scratch for faster access
-rRNAIndex=/work/LAS/mash-lab/usingh/riboSeq/sampleData/humanData/bowtieIndex/rRNAindex
-STARindex=/work/LAS/mash-lab/usingh/riboSeq/sampleData/humanData/human_STARindex
-RiboCodeAnnot=/work/LAS/mash-lab/usingh/riboSeq/sampleData/humanData/RiboCode_annot
-proc=16
-
+rRNAIndex=PATH/TO/rRNAindex
 
 #Step 1: Convert .SRA files to FASTQ. Use fastq-dump to control quality
 file_dir=$1
@@ -48,12 +44,16 @@ done
 echo "finally waiting for fastq-dump to finish..."
 wait
 
+#Step 2: Trimming adapter sequence for Ribo-Seq data
+
+cutadapt -m 20 --match-read-wildcards -a <Adapter_sequence.fastq> -o <Trimmed.fastq> <Input.fastq>
+
 ########Check if files are RPF not RNA seq############
 
-#Step 2: Remove rRNA reads from FASTQ using bowtie or Sortmerna
+#Step 3: Remove rRNA reads from FASTQ using bowtie or Sortmerna
 echo "Filtering rRNA using bowtie2"
 
-file_list=($file_dir/*pass_1.fastq)
+file_list=$1 #list includes the input files 
 
 #run for all fastq files
 for f in "${file_list[@]}"; do
@@ -78,34 +78,10 @@ done
 
 
 
-#output of above step is files with no alignment to rRNA ending with name _norRNA.fastq
+#Step 4: quantification by kallisto
 
-
-#Step 3: Align clean reads to human genome using STAR
-
-echo "Running Star..."
-file_list=($file_dir/*_norRNA.fastq)
-
-for f in "${file_list[@]}"; do
-        this_fname=$(echo "$f" | rev | cut -d"/" -f1 | rev | cut -d"." -f1)
-        echo $this_fname
-	
-	echo "STAR --outFilterType BySJout --runThreadN 8 --outFilterMismatchNmax 2 --genomeDir $STARindex --readFilesIn $f --outFileNamePrefix "$file_dir/$this_fname" --outSAMtype BAM SortedByCoordinate --quantMode TranscriptomeSAM GeneCounts --outFilterMultimapNmax 1 --outFilterMatchNmin 16 --alignEndsType EndToEnd"
-	STAR --outFilterType BySJout --runThreadN 8 --outFilterMismatchNmax 2 --genomeDir $STARindex --readFilesIn $f --outFileNamePrefix "$file_dir/$this_fname" --outSAMtype BAM SortedByCoordinate --quantMode TranscriptomeSAM GeneCounts --outFilterMultimapNmax 1 --outFilterMatchNmin 16 --alignEndsType EndToEnd
-
-        if [ $? -ne 0 ]; then
-                fail_flag=true
-                echo "FAILED STAR FOR " "$this_fname"
-                echo "$this_fname" >> "$file_dir"/failed_star.log
-                failed_salmon+=("$this_fname")
-                continue
-        fi
-        #remove unwanted files
-        rm -f "$f"
-	#rm -f 
-done
-
-#output of above step is BAM files with name "$this_fnamei"  SRR1234_pass_1_norRNAAligned.toTranscriptome.out.bam
-
-#exit
+#make kallisto index 
+kallisto index -i <index file> --make-unique <transcriptome file>
+#quantification
+kallisto quant -i <index file> --single -l 200 -s 20 -b 100 -o <output folder> <input Ribo-Seq.fastq>
 
