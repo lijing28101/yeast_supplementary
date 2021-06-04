7bb8df67fde01b736a293a7e6cb6e8ee40be1d63
lijing28101 <jingli@iastate.edu>
2021-06-04 13:52:30 -0500

    Update kallisto.sh

--- code/kallisto.sh
+++ code/kallisto.sh
@@ -2,9 +2,9 @@
 module load kallisto
 
 # make index, only need to run once, input "allcds.fasta" is the fasta file including all annotated genes and unannotated ORFs.
-# kallisto index -i yeast.allcds allcds.fasta
+# kallisto index -i yeast.allcdna allcdna.fasta
 
 out=out_new/$1
 mkdir -p $out
-kallisto quant -i yeast.allcds -b 100 -o $out $1_1.fastq.gz $1_2.fastq.gz 
+kallisto quant -i yeast.allcdna -b 100 -o $out $1_1.fastq.gz $1_2.fastq.gz 
 rm $1*fastq.gz
